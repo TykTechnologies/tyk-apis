38dd7d439ef8d314ba098ec6bf9bbb0a8433ce32
gernest <geofreyernest@live.com>
2021-01-21 13:11:30 +0300

    add marker tags

--- open3/types.go
+++ open3/types.go
@@ -2,13 +2,13 @@ package open3
 
 //+o:convert
 type Contact struct {
-	Name  string `json:"name,omitempty" yaml:"name,omitempty"`
-	URL   string `json:"url,omitempty" yaml:"url,omitempty"`
-	Email string `json:"email,omitempty" yaml:"email,omitempty"`
+	Name  string `marker:",optional" json:"name,omitempty" yaml:"name,omitempty"`
+	URL   string `marker:"url,optional" json:"url,omitempty" yaml:"url,omitempty"`
+	Email string `marker:",optional" json:"email,omitempty" yaml:"email,omitempty"`
 }
 
 //+o:convert
 type License struct {
 	Name string `json:"name" yaml:"name"` // Required
-	URL  string `marker:"url,omitempty" json:"url,omitempty" yaml:"url,omitempty"`
+	URL  string `marker:"url,optional" json:"url,omitempty" yaml:"url,omitempty"`
 }
